1457b84b55a377818d36606ae4214741e7b0450a
Kriz-hub <k.m.dikkeschei@casema.nl>
2021-11-01 16:32:58 +0000

    edit matches is not working yet

--- app.py
+++ app.py
@@ -102,7 +102,8 @@ def delete_league(league_id):
 def get_matches():
     leagues = list(mongo.db.leagues.find().sort("league_name", 1))
     clubs = list(mongo.db.clubs.find().sort("club_name", 1))
-    return render_template("matches.html", leagues=leagues, clubs=clubs)
+    matches = list(mongo.db.matches.find().sort("league_name", 1))
+    return render_template("matches.html", matches=matches, leagues=leagues, clubs=clubs)
 
 
 @app.route("/add_matches", methods=["GET", "POST"])
@@ -110,7 +111,7 @@ def add_matches():
     if request.method == "POST":
         match = {
             "league_name": request.form.get("league_name"),
-            "match-date": request.form.get("match_date"),
+            "match_date": request.form.get("match_date"),
             "club1_name": request.form.get("club1_name"),
             "club2_name": request.form.get("club2_name"),
             "club1_score": request.form.get("club1_score"),
@@ -123,6 +124,32 @@ def add_matches():
     leagues = list(mongo.db.leagues.find().sort("league_name", 1))
     clubs = list(mongo.db.clubs.find().sort("club_name", 1))
     return render_template("matches_add.html", leagues=leagues, clubs=clubs)
+
+
+@app.route("/edit_match/<match_id>", methods=["GET", "POST"])
+def edit_match(match_id):
+    if request.method == "POST":
+        submit = {
+            "league_name": request.form.get("league_name"),
+            "match_date": request.form.get("match_date"),
+            "club1_name": request.form.get("club1_name"),
+            "club2_name": request.form.get("club2_name"),
+            "club1_score": request.form.get("club1_score"),
+            "club2_score": request.form.get("club2_score")
+        }
+        mongo.db.matchs.update({"_id": ObjectId(match_id)}, submit)
+        flash("match Successfully Updated")
+        return redirect(url_for("get_matchs"))
+
+    match = mongo.db.matches.find_one({"_id": ObjectId(match_id)})
+    return render_template("matches_edit.html", match=match)  
+
+
+@app.route("/delete_match/<match_id>")
+def delete_match(match_id):
+    mongo.db.matches.remove({"_id": ObjectId(match_id)})
+    flash("Match Successfully Deleted")
+    return redirect(url_for("get_matches"))
 
 
 if __name__ == "__main__":
