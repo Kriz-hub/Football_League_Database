1295aa037c0956b5a930e6d46e6a544f2982c577
Kriz-hub <k.m.dikkeschei@casema.nl>
2021-11-14 17:30:07 +0000

    errors solved ranking calc, but not ready

--- app.py
+++ app.py
@@ -258,6 +258,7 @@ def show_ranking():
              if match ["club2_score"] < match ["club1_score"]:
                  club ["total_lost"] = club ["total_lost"] + 1
        clubs [nr]=club
+    club_amount = nr + 1
     nr = -1
     nr_sorted = -1
     ranked_clubs = clubs
@@ -267,7 +268,7 @@ def show_ranking():
             nr_b = 0
             while_ready = False
             if nr_sorted > -1:
-              while (nr_b <= nr_sorted) and (while_ready == False):
+              while (nr_b < nr_sorted) and (while_ready == False):
                 points_a = club ["total_points"]
                 ranked_club = ranked_clubs [nr_b]
                 points_b = ranked_club ["total_points"]
@@ -285,10 +286,20 @@ def show_ranking():
                       while_ready = True
                 if while_ready == False: nr_b = nr_b + 1
               
-              for nr_c in range(nr_sorted + 1, nr_b + 1, -1):
-                ranked_clubs [nr_c] = ranked_clubs [nr_c-1]
+              if nr_b < nr_sorted:
+                for nr_c in range(nr_sorted + 1, nr_b, -1):
+                  ranked_clubs [nr_c] = ranked_clubs [nr_c-1]
+              else:
+                nr_b = nr_sorted + 1
             nr_sorted = nr_sorted + 1
-            ranked_clubs [nr_b] = clubs [nr]    
+            ranked_clubs [nr_b] = clubs [nr]  
+    b = club_amount - 1
+    print ("club_amount:", club_amount)
+    print ("nr_sorted:", nr_sorted)
+    for a in range(nr_sorted + 1, club_amount): 
+        print ("b: ", b)
+        ranked_clubs.pop(b)
+        b = b - 1
     return render_template("rankings_show.html", leagues=leagues, ranked_clubs=ranked_clubs, matches=matches)
 
 
